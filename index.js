1dcf708e059aa25b9adddaae96844ea354c21291
mdsajalali <mdsajalali2020@gmail.com>
2024-05-17 15:58:54 +0600

    data get functionality added

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(cors());
 // Database Connection With MongoDB
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/karbar");
+    await mongoose.connect(
+      "mongodb+srv://karbar:karbar@cluster0.u5pbhxs.mongodb.net/karbar?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
@@ -19,9 +21,48 @@ const connectToDatabase = async () => {
 };
 connectToDatabase();
 
+// Schema for Creating Products
+const productSchema = new mongoose.Schema({
+  tag: String,
+  image: String,
+  name: String,
+  title: String,
+  price: Number,
+  rating: Number,
+  reviews: Number,
+  category: String,
+  size: String,
+  sizes: String,
+  color: String,
+  desc: String,
+});
+
+const Product = mongoose.model("Product", productSchema);
+
 // API CREATION
 app.get("/", (req, res) => {
   res.send("Express App is Running");
+});
+
+app.get("/getproduct", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve products" });
+  }
+});
+
+// POST endpoint to add a product
+app.post("/addproduct", async (req, res) => {
+  try {
+    const newProduct = req.body;
+    const product = new Product(newProduct);
+    await product.save();
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to add product" });
+  }
 });
 
 app.listen(port, (error) => {
@@ -31,5 +72,3 @@ app.listen(port, (error) => {
     console.log("Error: " + error);
   }
 });
-
-
